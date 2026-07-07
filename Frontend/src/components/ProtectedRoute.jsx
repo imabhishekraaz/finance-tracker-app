1184a3643a8896a6b0230ae4ba62e047ccad5a09
ffabhishek116@gmail.com <ffabhishek116@gmail.com>
2026-07-07 20:03:32 +0530

    fix: Naviagte error

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import {  Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
+import { Navigate } from 'react-router-dom';
 
 
 const ProtectedRoute = () => {
